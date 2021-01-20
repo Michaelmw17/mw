ae7f2dc67a361b3da12d1a333442f6118c4dee3b
Michaelmw17 <Michaelmw17@outlook.com>
2021-01-21 03:45:04 +1100

    minior icon grid fixes

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,20 +14,22 @@ class Contact extends Component {
             <div className="header-text">
               <h1>Full Stack Web Developer</h1>
                 <hr/>
-              <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | Node | Express | MongoDB | Git/Github | jQuery | APIs | JSON | AJAX</p>
+              <p> HTML/CSS | Bootstrap | JavaScript | React | React Native | Node | Express | MongoDB | Git/Github | jQuery | APIs | JSON | AJAX </p>
               
               <div className="my-links">
                 <Grid className="demo-grid-3">
-        <Cell col={6} tablet={8}> {/* LinkedIn */}
-          <a href="https://www.linkedin.com/in/michael-watt-6a76961b3/" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
-          </a></Cell>
-        <Cell col={4} tablet={6}>{/* Github */}
-          <a href="https://github.com/Michaelmw17/" rel="noopener noreferrer" target="_blank">
-            <i className="fa fa-github-square fa-4x" aria-hidden="true" />
-                    </a></Cell>
-        <Cell col={2} phone={4}></Cell>
-    </Grid>
+                  <Cell col={6} tablet={8}> {/* LinkedIn */}
+                    <a href="https://www.linkedin.com/in/michael-watt-6a76961b3/" rel="noopener noreferrer" target="_blank">
+                      <i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
+                    </a>
+                  </Cell>
+                  <Cell col={4} tablet={6}>
+                    {/* Github */}
+                    <a href="https://github.com/Michaelmw17/" rel="noopener noreferrer" target="_blank">
+                      <i className="fa fa-github-square fa-4x" aria-hidden="true" />
+                              </a></Cell>
+        {/* <Cell col={2} phone={4}></Cell> */}
+                </Grid>
           
           {/* Freecodecamp */}
           {/* <a href="http://google.com" rel="noopener noreferrer" target="_blank">
@@ -38,14 +40,11 @@ class Contact extends Component {
           {/* <a href="http://google.com" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-youtube-square" aria-hidden="true" />
           </a> */}
-                  
-                </div>
-              
-            </div>
-          
-          </Cell>
-        </Grid>
-      </div>
+                    </div>
+                  </div>
+              </Cell>
+            </Grid>
+          </div>
       </Bounce>
       
     )
