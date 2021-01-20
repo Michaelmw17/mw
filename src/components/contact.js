a5906671260b8879714cf67913139c989187bf11
Michaelmw17 <Michaelmw17@outlook.com>
2021-01-21 03:10:01 +1100

    Updates for contact and landing page

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,16 +14,21 @@ class Contact extends Component {
             <div className="header-text">
               <h1>Full Stack Web Developer</h1>
                 <hr/>
-              <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
-        <div className="my-links">
-          {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+              <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | Node | Express | MongoDB | Git/Github | jQuery | APIs | JSON | AJAX</p>
+              
+              <div className="my-links">
+                <Grid className="demo-grid-3">
+        <Cell col={6} tablet={8}> {/* LinkedIn */}
+          <a href="https://www.linkedin.com/in/michael-watt-6a76961b3/" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
-          </a>
-          {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+          </a></Cell>
+        <Cell col={4} tablet={6}>{/* Github */}
+          <a href="https://github.com/Michaelmw17/" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-github-square fa-4x" aria-hidden="true" />
-          </a>
+                    </a></Cell>
+        <Cell col={2} phone={4}></Cell>
+    </Grid>
+          
           {/* Freecodecamp */}
           {/* <a href="http://google.com" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-free-code-camp" aria-hidden="true" />
@@ -33,8 +38,9 @@ class Contact extends Component {
           {/* <a href="http://google.com" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-youtube-square" aria-hidden="true" />
           </a> */}
-
-              </div>
+                  
+                </div>
+              
             </div>
           
           </Cell>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -16,16 +16,22 @@ class Landing extends Component {
                 <Zoom>
               <h1>Full Stack Web Developer</h1>
             <hr/>
-            <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
-          <div className="social-links">
-          {/* LinkedIn */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+            <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | Node | Express | MongoDB | Git/Github | jQuery | APIs | JSON | AJAX</p>
+                  <div className="social-links">
+                    
+                      <Grid className="demo-grid-3">
+        <Cell col={6} tablet={8}> {/* LinkedIn */}
+          <a href="https://www.linkedin.com/in/michael-watt-6a76961b3/" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-linkedin-square" aria-hidden="true" />
-          </a>
-          {/* Github */}
-          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+          </a></Cell>
+        <Cell col={4} tablet={6}>{/* Github */}
+          <a href="https://github.com/Michaelmw17/" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-github-square" aria-hidden="true" />
-                    </a>
+                    </a></Cell>
+        <Cell col={2} phone={4}></Cell>
+    </Grid>
+         
+          
           
           {/* Freecodecamp */}
           {/* <a href="http://google.com" rel="noopener noreferrer" target="_blank">
