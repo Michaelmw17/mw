c60316d5043150f21b7983b02aa7c2e9ce9c91a9
Michaelmw17 <Michaelmw17@outlook.com>
2021-01-21 14:12:51 +1100

    Suspense loading

--- src/components/myComp/myComp.js
+++ src/components/myComp/myComp.js
@@ -2,6 +2,6 @@ import React from "react";
 
 
 export default () => {
-  return <img lazy='loading' src={process.env.PUBLIC_URL + '/Images/circle-cropped.png'} alt="Logo" style={{ height: '200px' }}/>;
+  return <img src={process.env.PUBLIC_URL + '/Images/circle-cropped.png'} alt="Logo" style={{ height: '200px' }}/>;
 };
 
