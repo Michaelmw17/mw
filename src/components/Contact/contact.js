d219ba4ea5ae5d074b85bfc58c167416d075c412
Michaelmw17 <Michaelmw17@outlook.com>
2021-01-21 14:18:59 +1100

    Icons contact page

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -23,15 +23,36 @@ class Contact extends Component {
                       <i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
                     </a>
                   </Cell>
-                  <Cell col={4} tablet={6}>
+                  <Cell col={6} tablet={8}>
                     {/* Github */}
                     <a href="https://github.com/Michaelmw17/" rel="noopener noreferrer" target="_blank">
                       <i className="fa fa-github-square fa-4x" aria-hidden="true" />
-                              </a></Cell>
+                    </a>
+                  </Cell>
+                  <Cell col={6} tablet={8}>
+                    {/* Github */}
+                    <a href="tel:+61449620082">
+                      <i className="fa fa-phone fa-4x" aria-hidden="true" />
+                    </a>
+                  </Cell>
+                  <Cell col={6} tablet={8}>
+                    {/* Github */}
+                    <a href ="mailto:michaelmw17@outlook.com">
+                      <i className="fa fa-envelope fa-4x" aria-hidden="true" />
+                    </a>
+                  </Cell>
         {/* <Cell col={2} phone={4}></Cell> */}
                 </Grid>
           
-          {/* Freecodecamp */}
+                {/* Freecodecamp */}
+                {/* <Cell  col={1}><a style={{color: 'black'}}  smooth={true} href = "https://www.google.com/maps/place/Sydney+NSW/data=!4m2!3m1!1s0x6b129838f39a743f:0x3017d681632a850?sa=X&ved=2ahUKEwj56oj83KzsAhWBA3IKHZFTBkAQ8gEwH3oECDAQBA">Sydney NSW</a></Cell>
+            <h5>Phone</h5>
+            <Cell col={1}><a href="tel:+61449620082"> <i className="fa fa-phone fa-2x" aria-hidden="true"/></a></Cell>
+            <h5>Email</h5>
+            <Cell col={1}><a href ="mailto:michaelmw17@outlook.com"> <i className="fa fa-envelope fa-2x" aria-hidden="true"/></a></Cell>
+            <h5>Site</h5>
+            <a href='http://michaelmw17.github.io/mw/'> <i className="fa fa-globe fa-2x" aria-hidden="true"/></a>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/> */}
           {/* <a href="http://google.com" rel="noopener noreferrer" target="_blank">
             <i className="fa fa-free-code-camp" aria-hidden="true" />
           </a> */}
